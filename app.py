c0e18ef5f129933efd49d2c02b834d692aef1ad7
sheldcoop <106275963+sheldcoop@users.noreply.github.com>
2025-10-10 10:11:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def main() -> None:
                 plot_bgcolor=PLOT_AREA_COLOR, paper_bgcolor=BACKGROUND_COLOR, shapes=plot_shapes,
                 legend=dict(title_font=dict(color=TEXT_COLOR), font=dict(color=TEXT_COLOR), x=1.02, y=1, xanchor='left', yanchor='top'),
                 hoverlabel=dict(bgcolor="#4A4A4A", font_size=14, font_family="sans-serif"),
-                height=600
+                height=800
             )
 
             if lot_number and quadrant_selection == Quadrant.ALL.value:
@@ -278,7 +278,7 @@ def main() -> None:
                 yaxis=dict(title="Count", title_font=dict(color=TEXT_COLOR), tickfont=dict(color=TEXT_COLOR)),
                 plot_bgcolor=PLOT_AREA_COLOR, paper_bgcolor=BACKGROUND_COLOR,
                 legend=dict(title_font=dict(color=TEXT_COLOR), font=dict(color=TEXT_COLOR)),
-                height=600
+                height=700
             )
             st.plotly_chart(fig, use_container_width=True)
         
@@ -413,7 +413,7 @@ def main() -> None:
                         plot_bgcolor=PLOT_AREA_COLOR,
                         paper_bgcolor=BACKGROUND_COLOR,
                         legend=dict(title="Verification Status", font=dict(color=TEXT_COLOR)),
-                        height=600
+                        height=700
                     )
                     st.plotly_chart(fig, use_container_width=True)
                 else:
